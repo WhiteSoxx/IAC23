963cbdff4673abe6ed59bee086889baabc802ff5
Scuffedwrldwide <kiko2004kiko@gmail.com>
2023-05-21 18:39:49 +0100

    updated encoding to utf-8

--- projeto.asm
+++ projeto.asm
@@ -1,8 +1,8 @@
 ; ******************************************************************************
 ; * IST-UL
 ; * Modulo:    teclado.asm
-; * Descrição: Exemplifica o acesso a um teclado.
-; *            Lê todas as linha do teclado, verificando se há alguma tecla
+; * DescriÃ§Ã£o: Exemplifica o acesso a um teclado.
+; *            LÃª todas as linha do teclado, verificando se hÃ¡ alguma tecla
 ; *            premida nessa linha.
 ; *
 ; ******************************************************************************
@@ -10,23 +10,23 @@
 ; ******************************************************************************
 ; * Constantes
 ; ******************************************************************************
-; ATENÇÃO: constantes hexadecimais que comecem por uma letra devem ter 0 antes.
-;          Isto não altera o valor de 16 bits e permite distinguir números de identificadores
-DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
-TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
-TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
-CUR_LIN    EQU 8       ; linha a testar (4ª linha, 1000b)
+; ATENÃ‡ÃƒO: constantes hexadecimais que comecem por uma letra devem ter 0 antes.
+;          Isto nÃ£o altera o valor de 16 bits e permite distinguir nÃºmeros de identificadores
+DISPLAYS   EQU 0A000H  ; endereÃ§o dos displays de 7 segmentos (perifÃ©rico POUT-1)
+TEC_LIN    EQU 0C000H  ; endereÃ§o das linhas do teclado (perifÃ©rico POUT-2)
+TEC_COL    EQU 0E000H  ; endereÃ§o das colunas do teclado (perifÃ©rico PIN)
+CUR_LIN    EQU 8       ; linha a testar (4Âª linha, 1000b)
 MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; ******************************************************************************
-; * Código
+; * CÃ³digo
 ; ******************************************************************************
 PLACE      0
 inicio:		
-; inicializações
-    MOV  R2, TEC_LIN   ; endereço do periférico das linhas
-    MOV  R3, TEC_COL   ; endereço do periférico das colunas
-    MOV  R4, DISPLAYS  ; endereço do periférico dos displays
+; inicializaÃ§Ãµes
+    MOV  R2, TEC_LIN   ; endereÃ§o do perifÃ©rico das linhas
+    MOV  R3, TEC_COL   ; endereÃ§o do perifÃ©rico das colunas
+    MOV  R4, DISPLAYS  ; endereÃ§o do perifÃ©rico dos displays
     MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
     MOV  R6, CUR_LIN   ; linha atual
 
@@ -36,26 +36,26 @@ ciclo:
     MOVB [R4], R1      ; escreve linha e coluna a zero nos displays
     MOV R6, 0          ; reinicia a linha a ser lida a 0
 
-espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
+espera_tecla:          ; neste ciclo espera-se atÃ© uma tecla ser premida
     SHL  R6, 1         ; incrementa
     MOV  R1, R6        ; testar a linha atual
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para além dos bits 0-3
-    CMP  R0, 0         ; há tecla premida?
+    MOVB [R2], R1      ; escrever no perifÃ©rico de saÃ­da (linhas)
+    MOVB R0, [R3]      ; ler do perifÃ©rico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para alÃ©m dos bits 0-3
+    CMP  R0, 0         ; hÃ¡ tecla premida?
     JZ   espera_tecla  ; se nenhuma tecla premida, repete
                        ; vai mostrar a linha e a coluna da tecla
     SHL  R1, 4         ; coloca linha no nibble high
     OR   R1, R0        ; junta coluna (nibble low)
     MOVB [R4], R1      ; escreve linha e coluna nos displays
     
-ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
+ha_tecla:              ; neste ciclo espera-se atÃ© NENHUMA tecla estar premida
     MOV  R1, R6        ; testar a linha atual  (R1 tinha sido alterado)
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para além dos bits 0-3
-    CMP  R0, 0         ; há tecla premida?
-    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera até não haver
+    MOVB [R2], R1      ; escrever no perifÃ©rico de saÃ­da (linhas)
+    MOVB R0, [R3]      ; ler do perifÃ©rico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para alÃ©m dos bits 0-3
+    CMP  R0, 0         ; hÃ¡ tecla premida?
+    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera atÃ© nÃ£o haver
     JMP  ciclo         ; repete ciclo
 
 
