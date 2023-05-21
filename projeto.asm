607a7720d3c0597584461a23cf567cfad4f4fa52
Scuffedwrldwide <kiko2004kiko@gmail.com>
2023-05-22 00:04:16 +0100

    leitura do teclado funcional

--- projeto.asm
+++ projeto.asm
@@ -15,7 +15,7 @@
 DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
 TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
-CUR_LIN    EQU 8       ; linha a testar (4ª linha, 1000b)
+LINHA_MAX  EQU 00010H  ; "teto" para a linha maxima a testar (4ª linha, 1000b)
 MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; ******************************************************************************
@@ -28,23 +28,26 @@ inicio:
     MOV  R3, TEC_COL   ; endereço do periférico das colunas
     MOV  R4, DISPLAYS  ; endereço do periférico dos displays
     MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-    MOV  R6, CUR_LIN   ; linha atual
+    MOV  R7, LINHA_MAX ; "teto" para linha maxima a testar (4ª linha, 1000b) 
 
 ; corpo principal do programa
 ciclo:
-    MOV  R1, 0 
-    MOVB [R4], R1      ; escreve linha e coluna a zero nos displays
-    MOV R6, 0          ; reinicia a linha a ser lida a 0
+    MOV  R1, 1         ; para guardar o valor da linha a ser testada
+    MOV  R6, 0         ; registo temporário da linha testada,
+    MOVB [R4], R6      ; escreve linha e coluna a zero nos displays
 
 espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
-    SHL  R6, 1         ; incrementa
-    MOV  R1, R6        ; testar a linha atual
     MOVB [R2], R1      ; escrever no periférico de saída (linhas)
     MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
     AND  R0, R5        ; elimina bits para além dos bits 0-3
+    CMP  R1, R7        ; se já testou as 4 linhas 
+    JZ   ciclo         ; volta à primeira
+    ROL  R1, 1         ; incrementa, apos testada, a linha atual
     CMP  R0, 0         ; há tecla premida?
     JZ   espera_tecla  ; se nenhuma tecla premida, repete
                        ; vai mostrar a linha e a coluna da tecla
+    ROR  R1, 1         ; regressa à linha testada
+    MOV  R6, R1        ; coloca coluna em R6, temporariamente
     SHL  R1, 4         ; coloca linha no nibble high
     OR   R1, R0        ; junta coluna (nibble low)
     MOVB [R4], R1      ; escreve linha e coluna nos displays
