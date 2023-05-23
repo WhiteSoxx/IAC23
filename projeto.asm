6165cd22f89900baf1055f9fac5165da44f8926f
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2023-05-23 19:35:02 +0100

    teclado funcional

--- projeto.asm
+++ projeto.asm
@@ -19,7 +19,12 @@ TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
 LINHA_MAX  EQU 00010H  ; "teto" para a linha maxima a testar (4ª linha, 1000b)
 MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-ENERGIA_BASE EQU 100   ; energia inicial
+ENERGIA_BASE EQU 000FFEH   ; energia inicial
+
+TEC_0      EQU 00011H  ; tecla 0
+TEC_1      EQU 00012H  ; tecla 1
+TEC_C      EQU 00081H  ; tecla C
+TEC_F      EQU 00088H  ; tecla F
 ; *********************************************************************************
 ; * Dados 
 ; *********************************************************************************
@@ -36,8 +41,8 @@ imagem_hexa:
 
 VAR_LINHA:  WORD 0      ; variável para guardar a linha atual
 VAR_COLUNA: WORD 0      ; variável para guardar a coluna atual
-VAR_LOGCOUNT: WORD -1   ; variável para guardar o logaritmo do contador
-VAR_ENERGIA: WORD 100   ; variável para guardar a energia
+VAR_TECCOUNT: WORD -1   ; variável para guardar o contador para conversão de teclas
+VAR_ENERGIA: WORD 000FFEH   ; variável para guardar a energia (ver constante ENERGIA_BASE)
 
 ; ******************************************************************************
 ; * Código
@@ -53,7 +58,6 @@ inicio:
     MOV  R7, LINHA_MAX ; "teto" para linha maxima a testar (4ª linha, 1000b) 
     MOV  R1, ENERGIA_BASE  ; inicializa a energia
     MOV  [VAR_ENERGIA], R1 ; inicializa a energia
-    MOV  R1, VAR_ENERGIA   ; inicializa o contador
     MOV  [R4], R1      ; inicializa o valor do display da energia
 
 ; corpo principal do programa
@@ -61,7 +65,7 @@ inicio:
 ; ciclo de detecção de teclas
 tec_ciclo:
     MOV  R1, 1         ; para guardar o valor da linha a ser testada
-    MOV  R6, 0         ; registo temporário da linha testada (linha anterior),
+    MOV  R8, 0         ; registo de ID de teclas
 
 espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
     MOVB [R2], R1      ; escrever no periférico de saída (linhas)
@@ -73,62 +77,69 @@ espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
     CMP  R0, 0         ; há tecla premida?
     JZ   espera_tecla  ; se nenhuma tecla premida, repete
                        ; vai mostrar a linha e a coluna da tecla
-    ROR  R1, 1         ; regressa à linha testada
-    MOV  R6, R1        ; coloca coluna em R6, temporariamente
-;    SHL  R1, 4         ; coloca linha no nibble high
-;    OR   R1, R0        ; junta coluna (nibble low)
-;    MOVB [R4], R1      ; escreve linha e coluna nos displays
-    
-ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
-    MOV  R1, R6        ; testar a linha atual  (R1 tinha sido alterado)
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para além dos bits 0-3
-    CMP  R0, 0         ; há tecla premida?
-    JNZ  testa_tecla   ; se ainda houver uma tecla premida, espera até não haver
-    JMP  tec_ciclo     ; repete ciclo
+    ROR  R1, 1         ; regressa à linha 
+    PUSH R1            ; guarda a linha atual na pilha
+    SHL  R1, 4         ; coloca linha no nibble high
+    OR   R1, R0        ; junta coluna (nibble low)
+    MOV [R4], R1       ; escreve linha e coluna nos displays
+    PUSH R1            ; guarda a linha e coluna na pilha
 
 testa_tecla:
-    SHL  R1, 1         ; multiplica por 2 (desloca 1 bit para a esquerda)
-    SHL R1, 2          ; multiplica por 4 (desloca 2 bits para a esquerda)
-    ADD R1, R0         ; soma coluna 
-    CMP R1, 0          ; se R1=0, não há tecla premida
-    JZ   tec_ciclo     ; volta a testar teclas
-                       ; "else if"
-    CMP R1, 4          ; se R1=4, tecla 1 premida
-    JZ dec_display
 
-    CMP R1, 5          ; se R1=5, tecla 2 premida
-    JZ inc_display 
+    POP R1             ; retira da pilha a linha e coluna da tecla premida
 
-    CMP R1, 6          ; se R1=6, tecla 3 premida
-    JZ sobe_sonda
+    MOV R8, TEC_0      ; coloca o ID da tecla 0 em R8
+    CMP R1, R8         ; se R1=4, tecla 1 premida
+    JZ  dec_display
 
-    CMP R1, 7          ; se R1=7, tecla 4 premida
-    JZ move_asteroide 
+    MOV R8, TEC_1      ; coloca o ID da tecla 1 em R8
+    CMP R1, R8         ; se R1=5, tecla 2 premida
+    JZ  inc_display 
 
-    JMP ha_tecla       ; volta a testar teclas
+    MOV R8, TEC_C      ; coloca o ID da tecla C em R8
+    CMP R1, R8         ; se R1=6, tecla 3 premida
+    JZ  sobe_sonda
 
+    MOV R8, TEC_F      ; coloca o ID da tecla F em R8
+    CMP R1, R8         ; se R1=7, tecla 4 premida
+    JZ  move_asteroide 
+
+    JMP ha_tecla       ; testa se a tecla permanece premida
+
+ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
+    POP R1             ; retira da pilha a linha atual
+    PUSH R1
+
+    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
+    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+
+    AND  R0, R5        ; elimina bits para além dos bits 0-3
+    CMP  R0, 0         ; há tecla premida?
+    JNZ  ha_tecla      ; se houver uma tecla premida, espera até não haver
+    POP R1             ; "limpa" da pilha a linha atual
+    JMP  tec_ciclo     ; se não houver, repete-se o ciclo de teste do teclado
 
 ; ações do teclado
-
+; CUIDADO C PUSH E POPS AQUI, DOIS BUÉ FRAGEIS NAS TRES TAGS ACIMA!!!
 move_asteroide:         ; TEMP!
-    JMP tec_ciclo       ; ação efetuada, testar teclado novamente
+    JMP ha_tecla        ; ação efetuada, testar teclado novamente
 
 sobe_sonda:             ; TEMP!
-    JMP tec_ciclo       ; ação efetuada, testar teclado novamente
+    JMP ha_tecla        ; ação efetuada, testar teclado novamente
 
 inc_display:            ; TEMP!
-    MOV R1, [R4]        ; coloca o valor do display em R1
-    ADD R1, 1           ; incrementa o valor do display
-    MOV [R4], R1        ; atualiza o valor do display
-    JMP tec_ciclo       ; ação efetuada, testar teclado novamente
+    MOV R10, [VAR_ENERGIA] ; coloca o valor do display em R1
+    ADD R10, 0001b           ; incrementa o valor do display
+    MOV [R4], R10        ; atualiza o valor do display
+    MOV [VAR_ENERGIA], R10 ; atualiza o valor da energia
+    JMP ha_tecla        ; ação efetuada, testar teclado novamente
 
 dec_display:            ; TEMP!
-    MOV R1, [R4]        ; coloca o valor do display em R1
-    SUB R1, 1           ; decrementa o valor do display
-    MOV [R4], R1        ; atualiza o valor do display
-    JMP tec_ciclo       ; ação efetuada, testar teclado novamente
+    MOV R10, [VAR_ENERGIA] ; coloca o valor do display em R1
+    SUB R10, 0001b           ; decrementa o valor do display
+    MOV [R4], R10        ; atualiza o valor do display
+    MOV [VAR_ENERGIA], R10 ; atualiza o valor da energia
+    JMP ha_tecla        ; ação efetuada, testar teclado novamente
 
 fim :                  ; fim do programa (ciclo infinito)
     JMP   fim
