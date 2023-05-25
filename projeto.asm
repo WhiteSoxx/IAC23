870cb8f46c135582e41fcb475c257ad2a90ffb18
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2023-05-25 01:47:32 +0100

    asteroide + reset_sondas

--- projeto.asm
+++ projeto.asm
@@ -75,6 +75,14 @@ VAR_COR_PIXEL: WORD COR_PIXEL ; variável para guardar a cor do pixel, default �
 VAR_COR_SONDA: WORD 0FFC0H    ; variável para guardar a cor da sonda, default é amarelo
 VAR_MSONDA_POS: WORD 25 ; variável para guardar a posição da sonda do meio (default é 25+1 (artefacto de desenha_sonda))
 
+VAR_AST_POS_0: WORD 0   ; variável para guardar a posição do asteroide 0
+VAR_AST_POS_1: WORD 0   ; variável para guardar a posição do asteroide 1
+VAR_AST_POS_2: WORD 0   ; variável para guardar a posição do asteroide 2
+VAR_AST_POS_3: WORD 0   ; variável para guardar a posição do asteroide 3
+VAR_AST_POS_4: WORD 3   ; variável para guardar a posição do asteroide 4
+
+VAR_POS_H_ALVO: WORD 0    ; variável para guardar a posição horizontal do objeto a desenhar
+VAR_POS_V_ALVO: WORD 0    ; variável para guardar a posição vertical do objeto a desenhar
 ; ******************************************************************************
 ; * Código
 ; ******************************************************************************
@@ -141,7 +149,7 @@ testa_tecla:
 
     MOV R8, TEC_F      ; coloca o ID da tecla F em R8
     CMP R1, R8         
-    JZ  move_asteroide 
+    JZ  move_asteroide_4
 
     JMP ha_tecla       ; testa se a tecla permanece premida
 
@@ -160,18 +168,61 @@ ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
 
 ; ações do teclado
 ; CUIDADO C PUSH E POPS AQUI, DOIS BUÉ FRAGEIS NAS TRES TAGS ACIMA!!!
-move_asteroide:         ; TEMP!
-    JMP ha_tecla        ; ação efetuada, testar teclado novamente
+move_asteroide_4:       ; TEMP!
+    MOV R10, [VAR_AST_POS_4]  ; coloca a posição do asteroide sup. direito em R10 
+    ADD R10, 1                ; incrementa a posição vertical do objeto (desce)
+    MOV [VAR_AST_POS_4], R10  ; atualiza a posição da objeto
+    
 
-sobe_sonda:                 ; TEMP!
-    MOV R10, 0              ; coloca o numero do som (0)
-    MOV [SELECIONA_VID], R10; seleciona o som
-    MOV [SELECIONA_VID], R10; toca o som
-    MOV R10, [VAR_MSONDA_POS]  ; coloca a posição da sonda do meio em R10 
+    SUB R10, 2
+    MOV [VAR_POS_V_ALVO], R10 ; atualiza a posição vertical do objeto a desenhar
+    PUSH R11                  ; guarda R11
+    PUSH R10                  ; guarda a posição vertical do objeto a desenhar na pilha
+    MOV R10, 63               ; coloca a largura em R10
+    POP R11                   ; recupera a posição vertical do objeto a desenhar da pilha
+    SUB R10, R11              ; atualiza a posição horizontal do objeto a desenhar
+    MOV [VAR_POS_H_ALVO], R10 ; atualiza a posição horizontal do objeto a desenhar
+    CALL apaga_asteroide      ; apaga o objeto na posição anterior
+
+    ADD R11, 1
+    MOV [VAR_POS_V_ALVO], R11 ; atualiza a posição vertical do objeto a desenhar
+    SUB R10, 1                ; atualiza a posição horizontal do objeto a desenhar
+    MOV [VAR_POS_H_ALVO], R10 ; atualiza a posição horizontal do objeto a desenhar
+    CALL desenha_asteroide    ; desenha o objeto
+    POP R11
+
+
+    PUSH R0
+    MOV R0, 0FF00H          ; coloca a cor do pixel em R0
+    MOV [VAR_COR_PIXEL], R0 ; atualiza a cor do pixel
+    CALL desenha_asteroide    ; desenha o objeto
+    POP R0
+
+    JMP ha_tecla              ; ação efetuada, testar teclado novamente
+
+sobe_sonda:                   ; TEMP!
+    MOV R10, 0                ; coloca o numero do som (0)
+    MOV [SELECIONA_VID], R10  ; seleciona o som
+    MOV [SELECIONA_VID], R10  ; toca o som
+    MOV R10, [VAR_MSONDA_POS] ; coloca a posição da sonda do meio em R10 
+    
+    PUSH R10
     SUB R10, 2                ; decrementa a posição vertical da sonda do meio (sobe)
     MOV [VAR_MSONDA_POS], R10 ; atualiza a posição da sonda do meio
     CALL desenha_sonda        ; desenha a sonda do meio na posição atual
-    JMP ha_tecla            ; ação efetuada, não testar teclado novamente
+    
+    POP R10
+    CMP R10, 1               ; se a sonda já estiver no topo
+    JZ reset_sonda
+
+    JMP ha_tecla              ; ação efetuada, não testar teclado novamente
+
+reset_sonda:
+    
+    MOV R10, 25               ; coloca a posição da sonda do meio em R10 
+    MOV [VAR_MSONDA_POS], R10 ; atualiza a posição da sonda do meio
+    CALL desenha_sonda        ; desenha a sonda do meio na posição atual
+    JMP ha_tecla              ; ação efetuada, não testar teclado novamente
 
 inc_display:                ; TEMP!
     MOV R10, [VAR_ENERGIA]  ; coloca o valor do display em R1
@@ -197,7 +248,7 @@ desenha_sonda:
 
     MOV R1, [VAR_MSONDA_POS]; coloca a posição vertical da sonda do meio em R1
     MOV R2, 32              ; coloca a posição horizontal da sonda do meio em R2 (constante 32)
-    MOV R3, [VAR_COR_SONDA]   ; coloca a cor da sonda do meio em R3
+    MOV R3, [VAR_COR_SONDA] ; coloca a cor da sonda do meio em R3
 
     CALL escreve_pixel      ; escreve o pixel na posição da sonda do meio
 ;    ADD R1, 1               ; decrementa a posição a desenhar (cauda da sonda)
@@ -211,6 +262,42 @@ desenha_sonda:
     POP R2
     POP R1                  ; recupera o valor de R1
     POP R0                  ; recupera o valor de R0
+    RET
+
+desenha_asteroide:
+    PUSH R0                 ; guarda o valor de R0
+    PUSH R1                 ; guarda o valor de R1
+    PUSH R2                 ; guarda o valor de R2
+    PUSH R3
+    MOV R1, [VAR_POS_V_ALVO]; coloca a posição vertical do objeto em R1
+    MOV R2, [VAR_POS_H_ALVO]
+    MOV R3, [VAR_COR_PIXEL] ; coloca a cor do objeto em R3
+    CALL escreve_pixel     
+    SUB R1, 1               ; decrementa a posição a desenhar 
+    SUB R2, 1               ; decrementa a posição a desenhar
+    CALL escreve_pixel      
+    ADD R2, 2               ; coloca em R2 a posição do objeto a apagar
+    CALL escreve_pixel      ; apaga o pixel na posição anterior do objeto
+    ADD R1, 2
+    CALL escreve_pixel
+    SUB R2, 2
+    CALL escreve_pixel
+;    ADD R1, 1               ; decrementa a posição a desenhar (cauda da sonda)
+;    CALL escreve_pixel      ; escreve o pixel na posição da sonda do meio
+;    SUB R1, 1               ; decrementa a posição a desenhar (cauda da sonda)
+;    CALL escreve_pixel      ; escreve o pixel na posição da sonda do meioå
+    POP R3
+    POP R2
+    POP R1                  ; recupera o valor de R1
+    POP R0                  ; recupera o valor de R0
+    RET    
+
+apaga_asteroide:
+    PUSH R0
+    MOV R0, 00000H
+    MOV [VAR_COR_PIXEL], R0
+    CALL desenha_asteroide
+    POP R0
     RET
 
 escreve_pixel:              ; ROTINA ASSUME REGISTOS LIMPOS, NUNCA CHAMAR DE FORMA AUTONOMA
